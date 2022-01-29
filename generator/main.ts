c2106581ef93580d188f9449a26423025ec2c3cc
Dimitri Gilbert <dimitri.gilbert@gmail.com>
2022-01-29 18:17:24 +0100

    main generator

---
+++ generator/main.ts
@@ -0,0 +1,20 @@
+import { promises as fs } from "fs";
+import * as eta from "eta";
+
+let stationName = "plop";
+
+fs.readFile(`./configs/${stationName}.json`)
+  .then(async (dataBuffer) => {
+    eta.configure({ autoEscape: false });
+    const data = JSON.parse(dataBuffer.toString());
+    await fs.writeFile(
+      `./src/${stationName}.main.cpp`,
+      (await eta.renderFile(
+        (await fs.realpath("./")) + "/templates/main.cpp.eta",
+        data
+      )) || ""
+    );
+  })
+  .catch((err) => {
+    console.error(err);
+  });
